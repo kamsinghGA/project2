95ee6c2d4b190a82cdb7571c4bdc82ec736c68bd
YuliaG1989 <yulia.glushenko@gmail.com>
2023-06-28 11:59:23 -0400

    yg

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const tradersRouter = require('./routes/traders');
 
 const app = express();
 
-let PORT = process.env.PORT || 3000;
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
